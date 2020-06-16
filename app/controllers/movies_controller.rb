b127c8d671dac7e59e8cc03a8c04837840e1bcd6
TingluoHuang <TingluoHuang@github.com>
2020-06-16 10:06:17 -0400

    c

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,12 +1,12 @@
 class MoviesController < ApplicationController
+    def index
+        movies = Movie.all
+        render :index, locals: { movies: movies }
+    end
+
     def show
         movie = Movie.find(params[:id])
         render :show, locals: { movie: movie }
-    end
-
-    def index
-        movies = Movie.all()
-        render :index, locals: { movies: movies }
     end
 
     def create
